a0ee4b5224259c522f83900a4aba7e24dfd77d30
fabriciobcv <fabriciobcvpessoal@gmail.com>
2024-05-17 14:34:15 -0300

    initial commit

---
+++ src/main/java/com/es2/baseexample/BaseExampleApplication.java
@@ -0,0 +1,13 @@
+package com.es2.baseexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/es2/baseexample/BaseExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.es2.baseexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
